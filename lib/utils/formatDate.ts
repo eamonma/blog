5581971737346a8d774eb7e3ba573be30d1c0cfd
Eamon Ma <m@eamonma.com>
2022-07-01 23:53:52 -0400

    Initial commit

---
+++ lib/utils/formatDate.ts
@@ -0,0 +1,13 @@
+import siteMetadata from '@/data/siteMetadata'
+
+const formatDate = (date: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric", month: "2-digit", day: "2-digit"
+  }
+  // fr-CA for ISO dates
+  const now = new Date(date).toLocaleDateString("fr-CA", options)
+
+  return now
+}
+
+export default formatDate
